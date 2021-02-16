17ff344d39cb5e7e5d5ab88e900207d3c64b4658
Arsenic <vector2912@gmail.com>
2021-02-16 17:42:06 +0530

    Initial commit

---
+++ public/js/all.js
@@ -0,0 +1,41 @@
+const deleteBtn = document.querySelectorAll(".delete");
+const showMore = document.querySelectorAll(".show-more");
+
+
+showMore.forEach((showMoreBtn) => {
+	showMoreBtn.addEventListener("click", async (e) => {
+		const baseURL = `/survey/id/${showMoreBtn.getAttribute('data-id')}?admin=true`;
+		// console.log(deleteButton.getAttribute('data-id'));
+		const data = await fetch(baseURL, { method: 'GET' })
+		const da = await data.text();
+		window.location = baseURL;
+		console.log(da);
+	});
+});
+
+deleteBtn.forEach((deleteButton) => {
+	deleteButton.addEventListener("click", async (e) => {
+		const baseURL = `/delete/survey/id/${deleteButton.getAttribute('data-id')}`;
+		// console.log(deleteButton.getAttribute('data-id'));
+		const data = await fetch(baseURL, { method: 'DELETE' });
+		const da = await data.text();
+		if (data.status === 200) {
+			window.location.reload();
+			showToast(da);
+		} else {
+			showToast("ERROR OCCURRED");
+		}
+		
+		console.log(da);
+	});
+});
+
+// Toast message
+function showToast(message) {
+	var x = document.getElementById("snackbar");
+	x.className = "show";
+	x.innerText = message;
+	setTimeout(function () {
+		x.className = x.classList.remove("show");
+	}, 2700);
+}
---
+++ survey.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// [+] MiddleWares [+]
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+// accept application/json format
+app.use(bodyParser.json());
+
+// MySQL config
+const pool = mysql.createPool({
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: 'arsenic',
+	database: 'felice_survey'
+});
+
+// Root
+app.get('', (req, res) => {
+	res.render('index');
+})
+
+// Get All Surveys
+app.get('/survey/all', (req, res) => {
+	if (req.query.admin === 'true') {
+		pool.getConnection((err, connection) => {
+			if (err) throw err;
+			console.log(`connected as id ${connection.threadId}`);
+
+			connection.query('SELECT * FROM surveys', (err, rows) => {
+				connection.release() // return the connection to the pool
+				// rows.forEach(survey => console.log(survey));
+				if (!err) {
+					// data = JSON.stringify(rows)
+					res.render('all', { data: rows });
+				} else {
+					console.log(err);
+					res.render('all', { err: 'Failed to load surveys' })
+				}
+			});
+		});
+	} else {
+		res.send("UNAUTHORIZED!")
+	}
+});
+
+
+// Get a survery by id
+app.get('/survey/id/:id', (req, res) => {
+	// Look for "?admin" parameter in the URL
+	const isAdmin = req.query.admin;
+	if (isAdmin === 'true') {
+			pool.getConnection((err, connection) => {
+			if (err) throw err;
+			console.log(`connected as id ${connection.threadId}`);
+
+			connection.query('SELECT * FROM surveys WHERE id = ?', [req.params.id], (err, rows) => {
+				connection.release();
+
+				if (!err) {
+					res.send(rows);
+				} else {
+					console.log("ERROR GETTING SURVERY BY ID: ", err);
+				}
+			});
+		});
+	} else {
+		res.send("Unauthorized!")
+	}
+
+});
+
+
+// Add a new survey
+app.post('/survey/add', (req, res) => {
+	// const body = req.body;
+	// console.log(body);
+	
+	pool.getConnection((err, connection) => {
+		if (err) throw err;
+		console.log(`connected as id ${connection.threadId}`);
+
+		const params = req.body;
+		console.log("PARAMS:", params);
+		connection.query('INSERT INTO surveys SET ?', params, (err, rows) => {
+			connection.release();
+
+			if (!err) {
+				res.render("succes");
+			} else {
+				console.log("ERRO ADDING NEW SURVEY: ", err);
+				res.send("There was an error adding your feedback!");
+			}
+		})
+	});
+});
+
+
+// Delete a survey
+app.delete("/delete/survey/id/:id", (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) throw err;
+		console.log(`connected as id ${connection.threadId}`);
+
+		connection.query("DELETE FROM surveys WHERE id = ?", [req.params.id], (err, rows) => {
+			connection.release(); 
+
+			if (!err) {
+				res.send(`Survey with the record id ${req.params.id} deleted`)
+				// res.redirect('/sruveys/all');
+			} else {
+				console.log("ERROR DELETING:", err);
+				// res.render('all', { message: 'Error Deleting the Survey!' });
+			}
+		})
+	})
+})
+
+// Send 404 if endpoint is invalid
+app.use((req, res) => {
+	res.status(404).render('404')
+})
+
+// Run the server
+app.listen(port, () => {
+	console.log('Listening on port 3000!');
+})
